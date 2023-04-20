b61aa0602c5338706c92b9e9e97cce3bbc3915f1
forckes <forckes@proton.me>
2023-04-20 19:23:19 +0300

    Add Box component

---
+++ src/components/Box/Box.jsx
@@ -0,0 +1,4 @@
+import styled from "styled-components";
+import { layout, space, flexbox, grid, color } from "styled-system";
+
+export const Box = styled("div")(space, layout, flexbox, grid, color);
